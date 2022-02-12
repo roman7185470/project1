e87dd3eb841bd2248bd5f48fbe91ea5bb7fd8cda
roman7185470 <lishuk.roman.electronics@gmail.com>
2022-02-12 22:39:45 +1000

    types

---
+++ src/task14/Poundsinkilo.java
@@ -0,0 +1,14 @@
+package task14;//6.	Написать программу, которая переводит фунты в килограммы.
+// Один фунт – 405.9 грамма задается в программе как константа.
+// Затем, реализовать обратный перевод – из килограммов в фунты.
+
+public class Poundsinkilo {
+    public static final double Pound = 405.9;
+
+    public static void main(String[] args) {
+        double  a = Pound ;
+
+        System.out.println (" 1 kilo :" + a );
+    }
+
+}
